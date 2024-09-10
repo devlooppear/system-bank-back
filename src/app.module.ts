1d4c4146d616d3eb1dfc72f30ca100702eb39f96
devlooppear <iago.profissional.developer@gmail.com>
2024-09-10 20:14:21 -0300

    feat: lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import * as dotenv from 'dotenv';
-import { PrismaService } from './prisma.service';
 import { UsersModule } from './models/users/users.module';
 import { AccountsModule } from './models/accounts/accounts.module';
 import { TransactionsModule } from './models/transactions/transactions.module';
@@ -27,9 +26,10 @@ dotenv.config();
   controllers: [AppController],
   providers: [AppService],
 })
-
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthMiddleware).exclude('auth/login').forRoutes('*');
+    consumer.apply(AuthMiddleware)
+    .exclude('auth/login')
+    .forRoutes('*');
   }
 }
