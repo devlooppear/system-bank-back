76a417038c96bab85c6f5a8e8cf3a1b4ed8bcb8b
devlooppear <iago.profissional.developer@gmail.com>
2024-09-10 07:44:34 -0300

    feat: defined prisma schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import * as dotenv from 'dotenv';
+import { PrismaService } from './prisma.service';
 
 dotenv.config();
 
@@ -13,7 +14,6 @@ dotenv.config();
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
-
 export class AppModule {}
