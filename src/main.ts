13fcefac157f97551e31de9d766bdc9e1525a422
devlooppear <iago.profissional.developer@gmail.com>
2025-08-19 16:36:10 -0300

    solving cors problems

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Environment } from './enum/environment.enum';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,17 +15,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
-
-  const environment =
-    (process.env.ENVIROMENT as Environment) || Environment.PRODUCTION;
-
-  if (environment === Environment.PRODUCTION) {
-    app.enableCors({
-      origin: 'https://system-bank.vercel.app',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-    });
-  }
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
