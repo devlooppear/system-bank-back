68f0e713559f0113edf249b49ec4a89564f05a86
devlooppear <iago.profissional.developer@gmail.com>
2025-08-19 16:30:10 -0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Environment } from './enum/environment.enum';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,28 +14,22 @@ async function bootstrap() {
     .setDescription('API for managing System Bank Test')
     .setVersion('1.0')
     .build();
-
-  const environment = process.env.ENVIROMENT || 'production';
-  const isDevelopment = environment === 'development';
-
-  app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || ['http://localhost:5173', 'http://localhost:5174'].includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
+  const environment =
+    (process.env.ENVIROMENT as Environment) || Environment.PRODUCTION;
+
+  if (environment === Environment.PRODUCTION) {
+    app.enableCors({
+      origin: 'https://system-bank.vercel.app',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    });
+  }
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
-
   await app.listen(port);
 }
 
